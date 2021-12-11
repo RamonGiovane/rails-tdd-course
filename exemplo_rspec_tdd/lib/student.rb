e840d589f3046c631c74f7f1b54aea5cdf233333
RamonGiovane <ramon.giovane@gmail.com>
2021-12-10 22:38:47 -0300

    Lesson 44: Stubs Methods

--- exemplo_rspec_tdd/lib/student.rb
+++ exemplo_rspec_tdd/lib/student.rb
@@ -2,6 +2,14 @@ class Student
   attr_accessor :name, :email
 
   def has_finished?(course)
-    
+    nil
+  end
+
+  def foo(args)
+    nil
+  end
+
+  def bar
+    nil
   end
 end
--- exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
+++ exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
@@ -12,4 +12,40 @@ describe 'Stub' do
     # Este metodo nao foi implementado, mas por causa do stub espera-se que retorne true!!
     expect(student.has_finished?(course)).to be true
   end
+
+  it 'Argumentos dinâmicos' do
+    student = Student.new
+
+    allow(student).to receive(:foo) do |arg|
+      if arg == :hello
+        'olá'
+      elsif arg == :hi
+        'oi'
+      else 'nada'
+      end
+    end
+
+    expect(student.foo(:hello)).to eq('olá')
+    expect(student.foo(:hi)).to eq('oi')
+    expect(student.foo(:aloha)).to eq('nada')
+  end
+
+  it 'Qualquer instância de Classe' do
+    s1 = Student.new
+    s2 = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+
+    expect(s1.bar).to be true
+    expect(s2.bar).to be true
+  end
+
+
+  it 'Erros' do
+    s1 = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_raise(RuntimeError)
+
+    expect {s1.bar}.to raise_error RuntimeError
+  end
 end
