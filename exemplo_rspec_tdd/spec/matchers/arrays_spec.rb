86a1c74dea17c5901e46d02585f194b69f276a84
RamonGiovane <ramon.giovane@gmail.com>
2021-12-10 21:38:32 -0300

    Lesson 41: Tag Filters

--- exemplo_rspec_tdd/spec/matchers/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays_spec.rb
@@ -1,10 +1,16 @@
-describe Array.new([1,2,3]) do
+# Podemos criar tag filters: :collection ou type: 'collection'
+# E fazer filtros na hora de executar os testes com
+# rspec --tag 'tag' ou rspec -t 'tag'
+describe Array.new([1,2,3]), :collection do
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(1,2)
   end
   
-  it '#match_array/#contain_exactly' do
+  # Pode-se adicionar tags a testes especificos.
+  # Com 'rspec -t ~[tag]' podemos excluir os testes de uma tag
+  # Exemplo 'rspec -t ~slow'
+  it '#match_array/#contain_exactly', slow:true do
     expect(subject).to contain_exactly(1,2,3)
     expect(subject).to match_array([1,2,3])
   end
--- exemplo_rspec_tdd/spec/matchers/custom/custom_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/custom/custom_spec.rb
@@ -1,4 +1,4 @@
-Rspec::Matchers.define :be_a_multiple_of do |expected|
+RSpec::Matchers.define :be_a_multiple_of do |expected|
   match do |actual|
     actual % expected == 0
   end
