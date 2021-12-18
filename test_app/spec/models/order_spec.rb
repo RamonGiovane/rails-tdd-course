ccd203cd6442ec539f44df39f27ae80d1f7574a6
RamonGiovane <ramon.giovane@gmail.com>
2021-12-17 22:50:36 -0300

    Lesson 61: create_list

--- test_app/spec/models/order_spec.rb
+++ test_app/spec/models/order_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Order, type: :model do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it 'Tem 3 pedidos' do
+    # Para criar varios objetos com FactoryBot.
+    orders = create_list(:order, 3)
+    expect(orders.count).to eq(3)
+
+    # Podemos tambem sobrescrever os atributos
+    orders = create_list(:order, 3, description: 'Sobrescrevendo...')
+    expect(orders.count).to eq(3)
+  end
 end
