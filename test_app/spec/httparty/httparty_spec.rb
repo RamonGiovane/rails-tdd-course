810e231f4008f76969003b2cfd819ed2affe5aba
RamonGiovane <ramon.giovane@gmail.com>
2021-12-20 20:13:39 -0300

    Lesson 70: VCR com URIs nao deterministicas

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -9,8 +9,15 @@ describe '::HTTParty' do
   end
 
   # Podemos informar o cassette-name
-  it 'content-type (with cassette name)', vcr: {cassette_name: 'jsonplaceholder/posts'} do
+  it 'content-type (with cassette name)', vcr: { cassette_name: 'jsonplaceholder/posts' } do
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(%r{application/json})
+  end
+
+  it 'content-type (with different url and same body))',
+     vcr: { cassette_name: 'jsonplaceholder/posts/', match_requests_on: [:body] } do
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1, 2, 3, 4, 5].sample}")
     content_type = response.headers['content-type']
     expect(content_type).to match(%r{application/json})
   end
