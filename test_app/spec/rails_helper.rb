e8e1c733d50988cc2b93f9230b9f84e9402e96fa
RamonGiovane <ramon.giovane@gmail.com>
2021-12-25 12:48:48 -0300

    Lesson 76: Shoulda matchers

--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -5,7 +5,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -33,6 +33,14 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # Shoulda Matchers
+  Shoulda::Matchers.configure do |conf|
+    conf.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
   # FactoryBot
   config.include FactoryBot::Syntax::Methods
 
