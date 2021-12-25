6108411988aec8b24b0796ea0fa3ae9610d5d7cf
RamonGiovane <ramon.giovane@gmail.com>
2021-12-25 19:51:32 -0300

    Lesson 80: Testing Controllers (POST with user input)

--- test_app/spec/controllers/customers_controller_spec.rb
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -1,10 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController, type: :controller do
-  it 'responds sucessfuly' do
-    get :index
-    # be_success é uma alias de:
-    # expect(response). to have_http_status 200
-    expect(response).to be_success
+  describe 'as a Guest' do
+    context '#index' do
+      it 'responds sucessfuly' do
+        get :index
+        # be_success é uma alias de:
+        # expect(response). to have_http_status 200
+        expect(response).to be_success
+      end
+    end
+
+    context '#show' do
+      it 'responds 302 (not authorized)' do
+        customer = create(:customer)
+        get :show, params: { id: customer.id }
+
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
+
+  describe 'as Logged Member' do
+    before do
+      @member = create(:member)
+      @customer = create(:customer)
+    end
+
+    context '#create' do 
+      it 'with valid attributes' do
+        customer_params = attributes_for(:customer)
+        sign_in @member
+        exp = expect do
+          post :create, params: { customer: customer_params } 
+        end
+        exp.to change(Customer, :count).by 1
+      end
+    end
+
+    context '#show' do
+      it 'responds sucessfuly' do
+        sign_in @member
+        get :show, params: { id: @customer.id }
+        expect(response).to be_success
+      end
+
+      it 'renders :show template' do
+        sign_in @member
+        get :show, params: { id: @customer.id }
+        expect(response).to render_template(:show)
+      end
+    end
   end
 end
--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -32,7 +32,13 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+
 RSpec.configure do |config|
+
+  # Devise
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   # Shoulda Matchers
   Shoulda::Matchers.configure do |conf|
     conf.integrate do |with|
