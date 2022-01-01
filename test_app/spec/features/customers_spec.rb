bda0e1f4bf51ca4ca76d0a4fdbcd7dc3969275a1
RamonGiovane <ramon.giovane@gmail.com>
2022-01-01 18:53:54 -0300

    Lesson 85: Configurando Screeshot

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -13,7 +13,6 @@ RSpec.feature "Customers", type: :feature do
 
     # Tirar um screenshot da pagina
     page.save_screenshot('screenshot.png')
-    save_and_open_page
 
     expect(page).to have_current_path(customers_path)
   end
