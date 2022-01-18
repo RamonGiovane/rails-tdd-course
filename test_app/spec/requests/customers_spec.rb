b73fd0a13d6e1247980e92f6eb5af063921bba4d
RamonGiovane <ramon.giovane@gmail.com>
2022-01-18 00:00:22 -0300

    Lesson 94: Include Json Rspec Matchers

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Customers", type: :request do
-  describe "GET /customers" do
-    it "works! 200 OK" do
+RSpec.describe 'Customers', type: :request do
+  describe 'GET /customers' do
+    it 'works! 200 OK' do
       get customers_path
       expect(response).to have_http_status(200)
     end
 
-    it "index - JSON 200 OK" do
+    it 'index - JSON 200 OK' do
       get '/customers.json'
       pp response: response.body
       expect(response.body).to include_json([
-        id: 1,
-        name: 'Teste',
-        email: 'teste@teste',
-      ])
+                                              id: /\d/,
+                                              name: (be_kind_of String),
+                                              email: (be_kind_of String)
+                                            ])
     end
 
-    it "show - JSON 200 OK" do
-      get '/customers/1.json'
+    it 'show - JSON 200 OK' do
+      get '/customers/248166291.json'
       expect(response).to have_http_status(200)
-      expect(response.body).to include_json(id: 1)
+      expect(response.body).to include_json(id: /\d/)
     end
   end
 end
