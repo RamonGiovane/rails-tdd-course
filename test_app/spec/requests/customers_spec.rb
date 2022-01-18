eedb1842f0e91059a37980252a5f8f84ab85590e
RamonGiovane <ramon.giovane@gmail.com>
2022-01-18 00:32:08 -0300

    Lesson 95: POST- JSON

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe 'Customers', type: :request do
 
     it 'index - JSON 200 OK' do
       get '/customers.json'
-      pp response: response.body
       expect(response.body).to include_json([
                                               id: /\d/,
                                               name: (be_kind_of String),
@@ -21,6 +20,27 @@ RSpec.describe 'Customers', type: :request do
       get '/customers/248166291.json'
       expect(response).to have_http_status(200)
       expect(response.body).to include_json(id: /\d/)
+    end
+  end
+
+  describe 'POST/customers' do
+    it 'create - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { 'ACCEPT' => 'application/json' }
+
+      customers_params = attributes_for(:customer)
+
+      post '/customers.json', params: { customer: customers_params }, headers: headers
+      
+      puts response.body
+      puts customers_params
+      expect(response.body).to include_json({
+                                              id: /\d/,
+                                              name: customers_params[:name],
+                                              email: customers_params[:email]
+                                            })
     end
   end
 end
