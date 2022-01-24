9b6b64d9b956e1895476a982359a971c44569dee
RamonGiovane <ramon.giovane@gmail.com>
2022-01-24 19:41:35 -0300

    Lesson 103: Cadastro de Clientes Parte 1/4

--- tdd_app/spec/features/welcome_spec.rb
+++ tdd_app/spec/features/welcome_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 feature "Welcome", type: :feature do
   scenario 'Mostra a mensagem de boas vindas' do
-    visit('/')
+    visit(root_path) # o mesmo que usar '/'
     expect(page).to have_content('Bem-vindo')
+  end
+
+  scenario 'Verificar o link de cadastro de clientes' do
+    visit(root_path)
+    expect(page).to have_selector('ul li a')
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
   end
 end
