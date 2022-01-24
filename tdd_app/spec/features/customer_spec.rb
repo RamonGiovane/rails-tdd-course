09d2f1dfff7c5d374891b016ae77a2dda4958f3a
RamonGiovane <ramon.giovane@gmail.com>
2022-01-24 20:17:59 -0300

    Lesson 104: Cadastro de Clientes Parte 2/4

---
+++ tdd_app/spec/features/customer_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature "Customers", type: :feature do
+  scenario 'Verifica Link de Cadastro de Clientes' do
+    visit root_path
+    expect(page).to have_link('Cadastro de Clientes')
+  end
+
+  scenario 'Verifica Link de Novo Cliente' do
+    visit root_path
+    click_on('Cadastro de Clientes')
+    expect(page).to have_content('Listando Clientes')
+    expect(page).to have_link('Novo Cliente')
+  end
+
+  scenario 'Verifica Formulario de Novo Cliente' do
+    visit customers_path
+    click_on 'Novo Cliente'
+    
+    expect(page).to have_content('Novo Cliente')
+
+  end
+end
